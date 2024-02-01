179b82fddf75c2b2ea63dada6124ee6e75488485
DanyilT <drtymchuk@gmail.com>
2024-02-01 11:51:51 +0000

    Algorithm

--- Programming/Sem1/Lab1/Broken.java
+++
@@ -1,8 +0,0 @@
-public class Broken {
-	public static void main(String args[]) {
-		System.out.println("This program contains errors!");
-		System.out.println("You must fix all of the errors contained");
-		System.out.println("within this program before it will");
-		System.out.println("compile and run.");
-	}
-}
