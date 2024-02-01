5da70ebaa8ea8d6777f549b1aea44658edb52a10
DanyilT <drtymchuk@gmail.com>
2024-02-01 10:33:47 +0000

    Programming Semester 2

--- Year1/Programming/Sem1/Lab10/Question1.java
+++
@@ -1,8 +0,0 @@
-public class Question1 {
-    public static void main(String[] args) {
-        // ASCII values for row 6: 60-69
-        for (int i = 60; i <= 69; i++) {
-            System.out.print((char) i + " ");
-        }
-    }
-}
