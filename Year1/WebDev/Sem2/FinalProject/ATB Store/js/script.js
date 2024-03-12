4a32edd8261d90226e2b27e3ca7116173ad8e930
DanyilT <danytymchuk@gmail.com>
2024-03-12 15:01:17 +0000

    WebDev Semester 2

--- Year1/WebDev/Sem2/FinalProject/ATB Store/js/script.js
+++ Year1/WebDev/Sem2/FinalProject/ATB Store/js/script.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
     var modalContact = document.getElementById("contact-popup");
     var closeCart = document.getElementsByClassName("close-cart")[0];
     var closeContact = document.getElementsByClassName("close-contact")[0];
+
+    const hamburger = document.querySelector('.hamburger');
+    const nav = document.querySelector('header nav');
+
+    hamburger.addEventListener('click', function() {
+        nav.classList.toggle('active');
+    });
     
     btnCart.onclick = function() {
         modalCart.style.display = "block";
