604dd8609a3b7d974370d118b3d12f589664a0fc
DanyilT <danytymchuk@gmail.com>
2024-09-23 19:10:51 +0100

    Programing

---
+++ Year2/Sem1/Obj Oriented Analysis & Design/Lab1/Exercise1.java
@@ -0,0 +1,6 @@
+public class Exercise1 {
+	public static void main(String[] args) {
+		String message= "Java is working";
+		System.out.println(message);
+	}
+}
---
+++ Year2/Sem1/Obj Oriented Analysis & Design/Lab1/Exercise2.java
@@ -0,0 +1,19 @@
+public class Exercise1 {
+	public static void main(String[] args) {
+		Car car1 = new Car("qwerty", "qwerty");
+		Car car2 = new Car("idk", "purple");
+
+		System.out.println("First Car name is " + car1.name + " and colour = " + car1.colour);
+		System.out.printf("Second Car name is %s and colour is %s", car2.name, car2.colour);
+	}
+}
+
+class Car {
+	String name;
+	String colour;
+
+	Car(String name, String colour) {
+		this.name = name;
+		this.colour = colour;
+	}
+}
