902b90a0a9f3d603e6cf1099320950e2f5805648
DanyilT <drtymchuk@gmail.com>
2024-09-23 18:07:16 +0100

    Change Structure

--- Year1/Programming/Sem1/Lab7/Question2.java
+++
@@ -1,9 +0,0 @@
-public class Question2 {
-    public static void main(String[] args) {
-        System.out.println("6 Rolls of an Eight-sided Dice:");
-        for (int i = 1; i <= 6; i++) {
-            int roll = (int) (Math.random() * 8) + 1;
-            System.out.printf("Roll %d: %d\n", i, roll);
-        }
-    }
-}
--- Year1/WebDev/Sem2/Lab5/js/euroToUSD.js
+++
@@ -1,5 +0,0 @@
-function convertToUSD() {
-    var euroAmount = document.getElementById('euroAmount').value;
-    var usdAmount = euroAmount * 1.09;
-    document.getElementById('usdAmount').value = usdAmount.toFixed(2);
-}
