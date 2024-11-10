a5ddb77b31c457a0e6240db1660f0f978f761cfe
Dany <71641218+DanyilT@users.noreply.github.com>
2024-11-10 05:42:09 +0000

    Programming

--- Year1/Sem2/Fundamentals of Programming 2/Lab9/Employee.java
+++ Year1/Sem2/Fundamentals of Programming 2/Lab9/Employee.java
@@ -1,30 +1,13 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-public class Employee {
-    private String name;
+public class Employee extends Person {
     private double salary;
 
-    public Employee() {}
-
     public Employee(String name, double salary) {
-        this.name = name;
+        super(name);
         this.salary = salary;
-    }
-
-    public void put(DataOutputStream dout) throws IOException {
-        dout.writeUTF(name);
-        dout.writeDouble(salary);
-    }
-
-    public void get(DataInputStream din) throws IOException {
-        name = din.readUTF();
-        salary = din.readDouble();
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "name='" + name + '\'' + ", salary=" + salary + '}';
+        return super.toString() + ", Salary: " + salary;
     }
 }
