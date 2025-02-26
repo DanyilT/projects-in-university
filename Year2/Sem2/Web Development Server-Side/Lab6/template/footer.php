8453de79dbeece767ba803e9f383cfc4493c757d
Dany <71641218+DanyilT@users.noreply.github.com>
2025-02-26 01:56:03 +0000

    WebDev

--- Year2/Sem2/Web Development Server-Side/Lab6/template/footer.php
+++ Year2/Sem2/Web Development Server-Side/Lab6/template/footer.php
@@ -1,8 +1,6 @@
-<footer class="footer">
-    <p>Web dev serverside</p> <!-- code derived from https://github.com/mariofont -->
-</footer>
-
-</div>
-
+    <footer class="footer">
+        <p>Web dev serverside</p> <!-- code derived from https://github.com/mariofont -->
+    </footer>
+    </div>
 </body>
 </html>
